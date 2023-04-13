2abebb8c431273f5c4d9204e7d6acdd9ee4c83e9
Derick Willams Tavares da Silva <rm87472@estudante.fieb.edu.br>
2023-04-13 08:16:22 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador= 0;
+        while(contador < 10){
+            System.out.println("contador = " + contador);
+
+        // incremento
+            contador++;
+
+        }
+        //laço para faça
+        for(int x= 0; x <10; x++ ){
+            System.out.println("contador = " + x);
+        }
+
+    }
+}
