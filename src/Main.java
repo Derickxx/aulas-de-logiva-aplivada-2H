a144b33e604ec25cdadf68fb8a6e17aabd73dcc5
rm87472 <dericktavares2006@gmail.com>
2023-05-04 09:55:21 -0300

    aula so dia 04/05

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int contador= 0;
-        while(contador < 10){
-            System.out.println("contador = " + contador);
-
-        // incremento
-            contador++;
-
-        }
-        //laço para faça
-        for(int x= 0; x <10; x++ ){
-            System.out.println("contador = " + x);
-        }
-// laço enuanto faça
-        int cont = 1;
-        do{
-            //processo
-            System.out.println(cont);
-            cont++;
-        }while( cont <= 10);
-
-
+    //Aula introdutorio de Orientação de objeto
+        
     }
 }
